964b29569fe3f0d4fee26b7e6f0508558ae44a98
Lenander88 <167141130+Lenander88@users.noreply.github.com>
2025-11-10 14:51:14 +0000

    additional scripts

--- ScriptPad/W11_OOBEcmd.ps1
+++ ScriptPad/W11_OOBEcmd.ps1
@@ -128,9 +128,9 @@ PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Set-KeyboardLanguage.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Install-EmbeddedProductKey.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://check-autopilotprereq.osdcloud.ch
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Lenander88/OSDCloud/refs/heads/main/Set-KeyboardLanguage.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Lenander88/OSDCloud/refs/heads/main/Install-EmbeddedProductKey.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Lenander88/OSDCloud/refs/heads/main/AP-Prereq.ps1
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://github.com/Lenander88/OSDCloud/blob/main/Start-AutopilotOOBE.ps1
 Start /Wait PowerShell -NoL -C Start-OOBEDeploy
 Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/Lenander88/OSDCloud/refs/heads/main/TPM.ps1
