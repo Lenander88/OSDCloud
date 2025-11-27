a3d40c02b058df56e4930ec92dd5b89977369b12
Lenander88 <mats@lund-lenander.com>
2025-11-27 10:09:14 +0100

    corrected urls with raw

--- StartURL-Live-01_EDU.ps1
+++ StartURL-Live-01_EDU.ps1
@@ -124,10 +124,10 @@ if (-not (Test-Path $setupPath)) {
 Invoke-Expression $edu
 
 # Download SetupComplete.cmd
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Lenander88/OSDCloud/dev/SetupComplete.cmd' -OutFile "$setupPath\SetupComplete.cmd"
+Invoke-WebRequest -Uri 'https://github.com/lenander88/OSDCloud/raw/dev/SetupComplete.cmd' -OutFile "$setupPath\SetupComplete.cmd"
 
 # Download Install-LCU.ps1
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Lenander88/OSDCloud/dev/Install-LCU.ps1' -OutFile "$setupPath\Install-LCU.ps1"
+Invoke-WebRequest -Uri 'https://github.com/lenander88/OSDCloud/raw/dev/Install-LCU.ps1' -OutFile "$setupPath\Install-LCU.ps1"
 
 ##=======================================================================
 ##   Restart-Computer
